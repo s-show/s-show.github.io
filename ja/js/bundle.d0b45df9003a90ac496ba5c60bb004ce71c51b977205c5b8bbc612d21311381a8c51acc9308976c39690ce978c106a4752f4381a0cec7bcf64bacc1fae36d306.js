bdbb242d781b2cdcaf361ce89553a4561096db39
s-show <s-show@users.noreply.github.com>
2023-10-27 15:53:20 +0000

    deploy: a7f1b97e9449a37fb0e4765d3f491550d6af0a84

--- ja/js/bundle.d0b45df9003a90ac496ba5c60bb004ce71c51b977205c5b8bbc612d21311381a8c51acc9308976c39690ce978c106a4752f4381a0cec7bcf64bacc1fae36d306.js
+++ ja/js/bundle.d0b45df9003a90ac496ba5c60bb004ce71c51b977205c5b8bbc612d21311381a8c51acc9308976c39690ce978c106a4752f4381a0cec7bcf64bacc1fae36d306.js
@@ -10,7 +10,7 @@ const imageAltClass = 'img_alt';
 
 
 const defaultSiteLanguage = 'ja';
-const baseURL = 'https://kankodori-blog.com/';
+const baseURL = 'https://s-show.github.io';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
